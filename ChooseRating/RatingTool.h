e31d88ee9154a746a45a4b681b9cdfa7f71afd9c
梁立 <1249502291@qq.com>
2018-02-09 17:44:51 +0800

    first commit

---
+++ ChooseRating/RatingTool.h
@@ -0,0 +1,13 @@
+//
+//  RatingTool.h
+//  ChooseRating
+//
+//  Created by 梁立 on 2018/2/9.
+//  Copyright © 2018年 XLL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RatingTool : UIView
+
+@end
---
+++ ChooseRating/RatingTool.m
@@ -0,0 +1,21 @@
+//
+//  RatingTool.m
+//  ChooseRating
+//
+//  Created by 梁立 on 2018/2/9.
+//  Copyright © 2018年 XLL. All rights reserved.
+//
+
+#import "RatingTool.h"
+
+@implementation RatingTool
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
