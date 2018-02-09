63e6d1924a3d8d4447b7a01f64cfa55246afd8e5
梁立 <1249502291@qq.com>
2018-02-09 11:14:14 +0800

    Initial Commit

---
+++ ChooseRating/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ChooseRating
+//
+//  Created by 梁立 on 2018/2/9.
+//  Copyright © 2018年 XLL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
